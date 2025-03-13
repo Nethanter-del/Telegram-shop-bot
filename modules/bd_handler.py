0b7acd992a2fac4cf21b22fe9766bcf69d08198c
Nethanter.js <60553314+Nethanter-del@users.noreply.github.com>
2025-03-13 16:47:34 +0300

    Add initial bot implementation with database integration and configuration

---
+++ modules/bd_handler.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+# Developer: Nethanter-del
+# Github: https://github.com/Nethanter-del
+# Date Created: 2025-12-03
+# Version: 1.0.0
+
+# License: MIT License
+#
+# Copyright (c) 2025 Nethanter-del
+
+import asyncpg
+
+class bd:
+    def __init__(self, host, user, password, database, owner):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.owner = owner
+        self.conn = None
+
+    async def init_db(self):
+        try:
+            self.conn = await asyncpg.connect(host=self.host, user=self.user, password=self.password, database=self.database)
+            await self.create_tables()
+        except Exception:
+            print(f"CRITICAL:-Connection to {self.database} corrupted")
+            exit()
+    async def create_tables(self):
+        tables = '''CREATE TABLE IF NOT EXISTS public.products
+(
+    product_id SERIAL NOT NULL,
+    text COLLATE pg_catalog."default" NOT NULL,
+    product text COLLATE pg_catalog."default",
+    price bigint NOT NULL,
+    CONSTRAINT products_pkey PRIMARY KEY (product_id)
+)
+
+TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id SERIAL NOT NULL,
+    user_id bigint NOT NULL,
+    username text COLLATE pg_catalog."default",
+    is_admin boolean NOT NULL,
+    state text COLLATE pg_catalog."default",
+    balance bigint,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;'''
+        await self.conn.execute(tables)
+    # unformated response to dictionary        
+    async def format_response(self, values):
+        return  [dict(record) for record in values]
+    # create user
+    async def create_user(self, user_id, username):
+        is_admin = False
+        if str(user_id) == str(self.owner):
+            is_admin = True
+        await self.conn.execute("INSERT INTO users (user_id, username, is_admin) VALUES ($1, $2, $3)", user_id, username, is_admin)
+    # fetch user info   
+    async def get_user(self, user_id):
+        values = await self.conn.fetch(f"SELECT * FROM users WHERE user_id = {user_id}")
+        return await self.format_response(values)
+    
+    # check admin status True|False
+    async def check_admin(self, user_id):
+        values = await self.conn.fetch(f"SELECT * FROM users WHERE user_id = {user_id} AND is_admin = true")
+        if len(values) > 0:
+            return True
+        else:
+            return False
+    # fetch all admins 
+    async def select_admins(self):
+        values = await self.conn.fetch(f"SELECT user_id FROM users WHERE is_admin = True")
+        return await self.format_response(values)
+    # get user state polling|waiting|free
+    async def get_state(self, user_id):
+        values = await self.conn.fetch(f"SELECT state FROM users WHERE user_id = {user_id}")
+        return await self.format_response(values)
+    # set user state polling|waiting|free
+    async def set_state(self, user_id, state):
+        await self.conn.execute("UPDATE users SET state = $1 WHERE user_id = $2", state, user_id)
+    
